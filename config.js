acfd8e5bf0a8e77a656fca8121a3745da15bff9a
maryam170906 <83348429+maryam170906@users.noreply.github.com>
2022-06-29 18:50:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAjYO-wrdyTrYF-lOPX5tQXHrQPMcrQor4",
+    authDomain: "fruit-catcher-d3299.firebaseapp.com",
+    projectId: "fruit-catcher-d3299",
+    storageBucket: "fruit-catcher-d3299.appspot.com",
+    messagingSenderId: "790952777176",
+    appId: "1:790952777176:web:4f81ce29746ae9ff6a0ead"
+  };
